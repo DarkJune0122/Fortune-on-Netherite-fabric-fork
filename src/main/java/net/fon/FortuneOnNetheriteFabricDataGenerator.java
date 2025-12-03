306172bd94cd9dd93b5d9f7d864c6ef0ceaad600
DarkJune <56516330+DarkJune0122@users.noreply.github.com>
2025-12-03 05:55:31 +0200

    Preparing for deletion

---
+++ src/main/java/net/fon/FortuneOnNetheriteFabricDataGenerator.java
@@ -0,0 +1,11 @@
+package net.fon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FortuneOnNetheriteFabricDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
