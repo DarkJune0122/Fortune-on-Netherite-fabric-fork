3ac70df6345436fe247afda3415106afd0ffae91
DarkJune <56516330+DarkJune0122@users.noreply.github.com>
2025-12-03 07:23:30 +0200

    Repaired modpack

---
+++ src/main/java/net/fortuneonnetherite/ModLootTables.java
@@ -0,0 +1,26 @@
+package net.fortuneonnetherite;
+
+import net.fabricmc.fabric.api.loot.v3.*;
+import net.minecraft.block.*;
+import net.minecraft.loot.*;
+import net.minecraft.loot.entry.*;
+import net.minecraft.loot.provider.number.*;
+
+public class ModLootTables{
+    public static void initialize(){
+        LootTableEvents.REPLACE.register(((key, original, source, registries) -> {
+            if (!source.isBuiltin()) return null;
+            if (Blocks.ANCIENT_DEBRIS.getLootTableKey().equals(key)) {
+                LootTable.Builder fabricBuilder = LootTable.builder();
+
+                fabricBuilder.pool(LootPool.builder()
+                .rolls(ConstantLootNumberProvider.create(1))
+                .with(ItemEntry.builder(ModItems.RawDebris)));
+
+                return fabricBuilder.build();
+            }
+
+            return null;
+        }));
+    }
+}
